a60c0e0d15670830005a8dcb7f8c94a980835e3e
yamahaymh <lhnghia1@gmail.com>
2021-03-28 23:49:54 +0700

    3 group

--- facebook-scraper.py
+++ facebook-scraper.py
@@ -2,19 +2,31 @@ from facebook_scraper import get_posts
 import fire_store
 import regex_data as rg
 
-for post in get_posts (group='489801751369828',pages = 1):
+group1='2023588204354732' #BĐS Đồng Tháp có giá trị lớn
+group2='489801751369828'  # Nhà đất Đồng Tháp
+group3='279200489509507'  # Nhà đất Tp Cao Lãnh
+
+for post in get_posts (group=group3,pages = 10):
 
    text = post['text']
    postid = post['post_id']
-  # docs= fire_store.doc_ref_collection.stream()
-   if postid != None:
-      doc_ref = fire_store.doc_ref_collection.document(postid)
-      doc_ref.set({
-         'Giá' : rg.gia(text),
-         'SĐT' : rg.sdt(text),
-         'Kích thước' : rg.kichthuoc(text),
-         'Noidung' : text
-      })
-   else:
+   time = post['time']
+   url = "https://www.facebook.com/groups/"+group3+"/permalink/"+str(postid)
+
+   if rg.canmua(text):
       None
+   else:
+   # docs= fire_store.doc_ref_collection.stream()
+      if postid != None:
+         doc_ref = fire_store.doc_ref_collection.document(postid)
+         doc_ref.set({
+            '0_Thời gian': time,
+            '1_Giá' : rg.gia(text),
+            '3_SĐT' : rg.sdt(text),
+            '2_Kích thước' : rg.kichthuoc(text),
+            '4_Noidung' : text,
+            '5_URL': url
+         })
+      else:
+         None
       
--- regex_data.py
+++ regex_data.py
@@ -21,3 +21,10 @@ def kichthuoc (noidung):
         return kichthuoc.group()
     except:
         None
+
+def canmua (noidung):
+    try:
+        canmua = re.search(r"tìm|cần tìm|can tim|cần tim|can tìm|càn tìm|càn tim|cân tim|cân tìm|cần mua|can mua|cân mua|càn mua|cần mu|cần nền|can nen|càn nen|cân nen|can nền|can nèn|can nên|cần thuê|thuê|can thue|cân thue|cần nhà|can nhà|cân nhà|càn nhà|cần nhà|mua dat|mua đất|muadat|mua đát'",noidung,re.IGNORECASE)
+        return canmua.group()
+    except:
+        None
