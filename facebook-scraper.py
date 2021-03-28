c5ba1770ad11dcc5b202608242a5169e80a70bf8
yamahaymh <lhnghia1@gmail.com>
2021-03-28 23:52:59 +0700

    ok

--- facebook-scraper.py
+++ facebook-scraper.py
@@ -6,12 +6,12 @@ group1='2023588204354732' #BĐS Đồng Tháp có giá trị lớn
 group2='489801751369828'  # Nhà đất Đồng Tháp
 group3='279200489509507'  # Nhà đất Tp Cao Lãnh
 
-for post in get_posts (group=group3,pages = 10):
+for post in get_posts (group=group1,pages = 1):
 
    text = post['text']
    postid = post['post_id']
    time = post['time']
-   url = "https://www.facebook.com/groups/"+group3+"/permalink/"+str(postid)
+   url = "https://www.facebook.com/groups/"+group1+"/permalink/"+str(postid)
 
    if rg.canmua(text):
       None
